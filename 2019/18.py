cc58c33fc5b466d6d720d3a5d01ded33c2128e36
Xavier Moulet <xavier-frederic.moulet@orange.com>
2019-12-26 09:48:53 +0100

    18, WIP

--- 2019/18.py
+++ 2019/18.py
@@ -1,71 +1,56 @@
-import functools
-start_board = [ l.strip() for l in open('18.txt') ]
+import itertools
 
-# remove char in a board
-def pprint(board,x,y) : 
-	for l in replace(board,x,y,'@') : print(l)
-def remove(c,board) : 
-	return tuple( l.replace(c,'.') for l in board)
-def use_key(c,board) : 
-	return remove(c.upper(),remove(c,board))
-
-def replace(board,x,y,c) : 
-	return tuple(l if i != y else l[:x]+c+l[x+1:] for i,l in enumerate(board))
-
-@functools.lru_cache(maxsize=None)
-def find_keys(board,x,y) : 
+def reachable(board,x,y) : 
 	# return accessible keys from there + distance
 	keys = []
 	visited = []
-	dist = []	# correspond to preceding
-	frontier = [(x,y,0)]
-
-	def check(x,y) : return board[y][x]=='.' and (x,y) not in visited
+	frontier = [(x,y,0,'')] # x,y dist doors
 
 	while frontier : 
-		x,y,d  = frontier.pop(0)
+		x,y,d,doors  = frontier.pop(0)
 		visited.append((x,y))
 		for nx,ny in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)] :
-			if check(nx,ny) : 
-				frontier.append((nx,ny,d+1))
-			elif board[ny][nx].islower() : 
-				keys.append((nx,ny,d+1))
+			if (nx,ny) not in visited :
+				visited.append((nx,ny))
+				c = board[ny][nx]
+				if c!='#' :
+					ndoors = doors+c.lower() if c.isupper() else doors
+					frontier.append((nx,ny,d+1,ndoors))
+				if c.islower() : 
+					keys.append((c,d+1,doors))
 	return keys
 
-b=start_board
-# get starting position
-for y,l in enumerate(b) : 
-	if '@' in l : 
-		x = l.index('@')
-		break
-b = remove('@',b)
 
-frontier = {(b,x,y)}
-costs = {(b,x,y):0}
+# returns a list of key positions
+def where_keys(board): 
+	d = []
+	for y,l in enumerate(board) : 
+		for x,c in enumerate(l) : 
+			if c.islower() or c=='@' : 
+				d.append((x,y)) 
+	return d
 
-best=99999999999999999999
-i=0
-while frontier : 
-	i += 1
-	if i%100==0 : print('>',i,len(frontier))
-	b,x,y = frontier.pop()
-	c = costs[(b,x,y)]
+#load board, find keys on board
+start_board = [ l.strip() for l in open('18.txt') ]
+for l in start_board : print (l)
+allkeys = where_keys(start_board)
+reach = { start_board[y][x] : reachable(start_board,x,y) for x,y in allkeys } # (lettera,letterB) -> dist, doors
 
-	keys = find_keys(b,x,y)
-	if not keys :
-		remain = len(set(''.join(b)))
-		if remain != 2 : print('wat')
+for i in reach.items() : print (i)
 
-		if c<best : 
-			best=c
-			print('best',c)
-	else : 
-		for kx,ky,cost in keys : 
-			# remove key and door
-			nb=use_key(b[ky][kx],b)
-			state = (nb,kx,ky) # c+cost
-			# check if already seen better, skip
-			old_cost = costs.get(state,9999999999)
-			if c+cost < old_cost : 
-				costs[state] = c+cost
-				frontier.add(state)
+nb = len(allkeys)
+dmin = 9999999999999
+paths=[('@',0)] # keys so far / path, dist
+while paths : 	
+	path,d = paths.pop(0)
+	print(path,d)
+	if len(path) == nb : 
+		print(path,d)
+		if d<dmin : 
+			dmin = d
+			print(path,d,'*'*80)
+	nextkeys = sorted(reach[path[-1]],key=lambda x:x[1])
+	for nkey,dist,doors in nextkeys : # nearest first
+		if nkey not in path and all(k in path for k in doors) : 
+			if d+dist < dmin :
+				paths.append((path+nkey,d+dist))
